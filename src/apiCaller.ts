c64e01102bceb03f964ef51292e4882201243a9b
Cédric Taschereau <cedric.taschereau@boreal-is.com>
2020-03-04 22:38:47 -0500

    Implement calls that wake up the car and gets the main data

--- src/apiCaller.ts
+++ src/apiCaller.ts
@@ -1,16 +1,45 @@
 import * as https from 'https'
+import {RequestOptions} from "https";
 
 const TESLA_SERVER_HOSTNAME: string = 'owner-api.teslamotors.com';
 const TESLA_CLIENT_ID: string = '81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384';
 const TESLA_CLIENT_SECRET: string = 'c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3';
 
+const API_USER_AGENT = 'Mozilla/5.0 (Linux; Android 9.0.0; VS985 4G Build/LRX21Y; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36';
+const API_X_Tesla_User_Agent = 'TeslaApp/3.4.4-350/fad4a582e/android/9.0.0';
+
 // doc : https://www.teslaapi.io/  or  https://tesla-api.timdorr.com/
 
-type vcount = {
-	response: [],
-	count: number,
+type VehicleInfo = {
+    id: number;
+    vehicle_id: number;
+    vin: string;
+    display_name: string;
+    option_codes: string;
+    color: null;
+    tokens: string[];
+    state: 'online' | 'asleep' | 'offline';
+    in_service: boolean;
+    id_s: string;
+    calendar_enabled: boolean;
+    api_version: number;
+    backseat_token: any;
+    backseat_token_updated_at: any;
 };
-
+type BasicVehicleCallResponse = {
+    response: VehicleInfo;
+};
+type VehicleCountResponse = {
+    response: VehicleInfo[];
+    count: number;
+};
+type OAuthResponse = {
+    access_token: string;
+    token_type: 'bearer';
+    expires_in: number;
+    refresh_token: string;
+    created_at: number;
+};
 
 export class ApiCaller {
 
@@ -33,32 +62,43 @@ export class ApiCaller {
             password,
         });
         let options = this._getPostOptions(path, postData.length);
-        // TODO : Define the return type
-        let r = await this._call<any>(options, postData);
+        let r = await this._call<OAuthResponse>(options, postData);
         return r.access_token;
     }
 
     async getVehicleList() {
         const path = '/api/1/vehicles';
         let options = this._getGetOptions(path);
-		// TODO : Define the return type
-        return await this._call<vcount>(options);
+        return await this._call<VehicleCountResponse>(options);
     }
 
-    async _call<T>(options: object, postDataString?: string) {
+    async getVehicleData(id: string) {
+        const path = `/api/1/vehicles/${id}/vehicle_data`;
+        let options = this._getGetOptions(path);
+        // TODO : Define the return type
+        return await this._call<any>(options);
+    }
+
+    async wakeUp(id: string) {
+        const path = `/api/1/vehicles/${id}/wake_up`;
+        let options = this._getPostOptions(path);
+        return await this._call<BasicVehicleCallResponse>(options);
+    }
+
+    async _call<T>(options: RequestOptions, postDataString?: string) {
         return new Promise<T>((resolve, reject) => {
-            let payload: string = '';
+            let responsePayload: string = '';
             let req = https.request(options, (res) => {
-                console.log('statusCode:', res.statusCode);
+                //console.log('statusCode:', res.statusCode);
                 //console.log('headers:', res.headers);
 
                 res.setEncoding('utf8');
                 res.on('data', (d) => {
-                    payload += d;
+                    responsePayload += d;
                 });
 
                 res.on('end', () => {
-                    let jsonPayload = JSON.parse(payload);
+                    let jsonPayload = JSON.parse(responsePayload);
                     resolve(jsonPayload);
                 })
             });
@@ -80,24 +120,24 @@ export class ApiCaller {
         return options;
     }
 
-    _getPostOptions(path: string, contentLength: number) {
+    _getPostOptions(path: string, contentLength?: number) {
         let options = this._getCommonOptions(path);
         options.method = 'POST';
         options.headers['Content-Type'] = 'application/json';
-        options.headers['Content-Length'] = contentLength;
+        if (contentLength) {
+            options.headers['Content-Length'] = contentLength;
+        }
         return options;
     }
 
     _getCommonOptions(path: string) {
-        // TODO : define an interface RequestOptions
-        let options: any = {
+        let options: RequestOptions = {
             hostname: TESLA_SERVER_HOSTNAME,
             port: 443,
-            method: undefined as string,
             path,
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Linux; Android 9.0.0; VS985 4G Build/LRX21Y; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36',
-                'X-Tesla-User-Agent': 'TeslaApp/3.4.4-350/fad4a582e/android/9.0.0',
+                'User-Agent': API_USER_AGENT,
+                'X-Tesla-User-Agent': API_X_Tesla_User_Agent,
             }
         };
         if (this._accessToken) {
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import program from 'commander';
 import { ApiCaller } from './apiCaller';
 
 const ACCESS_TOKEN_FILEPATH: string = 'access_token.txt';
+const VEHICLE_ID_FILEPATH: string = 'vehicle_id.txt';
 
 
 let getTokenFromFile = async (): Promise<string> => {
@@ -18,6 +19,20 @@ let getTokenFromFile = async (): Promise<string> => {
         }
     }
     return _accessToken;
+};
+
+let getVehicleIdFromFile = async (): Promise<string> => {
+    let _vehicleID;
+    try {
+        _vehicleID = await fs.readFile(VEHICLE_ID_FILEPATH, 'utf8');
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error('No vehicle ID. getVehicleList should be called first.');
+        } else {
+            throw err;
+        }
+    }
+    return _vehicleID;
 };
 
 program.version('0.0.1');
@@ -38,7 +53,38 @@ program
         let token = await getTokenFromFile();
         let apiCaller = new ApiCaller(token);
         let result = await apiCaller.getVehicleList();
-        console.log(result);
+        if (result.count === 0) {
+            console.log('No vehicle yet.');
+            process.exit(0);
+        }
+        if (result.count > 1) {
+            console.error('More than one Tesla ; not implemented');
+            process.exit(1);
+        }
+        let carData = result.response[0];
+        await fs.writeFile(VEHICLE_ID_FILEPATH, carData.id_s);
+    });
+
+program
+    .command('wakeUp')
+    .description('Wakes up the specified vehicle')
+    .action(async () => {
+        let token = await getTokenFromFile();
+        let apiCaller = new ApiCaller(token);
+        let vehicleID = await getVehicleIdFromFile();
+        let result = await apiCaller.wakeUp(vehicleID);
+        console.log(result.response.state);
+    });
+
+program
+    .command('getVehicleData')
+    .description('Gets the data for the specified vehicle')
+    .action(async () => {
+        let token = await getTokenFromFile();
+        let vehicleID = await getVehicleIdFromFile();
+        let apiCaller = new ApiCaller(token);
+        let result = await apiCaller.getVehicleData(vehicleID);
+        console.log(JSON.stringify(result, null, 4));
     });
 
 async function main() {
